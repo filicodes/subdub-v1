b6b53960e29eefe02f7f582119e588b0b8c9e456
filicodes <nimeshadls8@gmail.com>
2025-03-03 23:09:59 +1030

    env config completedd

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
+console.log("asdadad", PORT);
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subdub API");
 });
 
-app.listen(3000, () => {
-  console.log("server running");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 export default app;
