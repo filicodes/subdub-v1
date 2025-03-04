fbb2179071937958a36b6feb68dc2bb5637cbd4c
filicodes <nimeshadls8@gmail.com>
2025-03-05 00:07:18 +1030

    applied routes in app.js and finished subs routes

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ import express from "express";
 
 import { PORT } from "./config/env.js";
 
+// importing routers
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 const app = express();
+
+//what has done below is in example -> if we want to visit /sign-up we have to go through /api/v1/auth like a middleware. in this case it will be /api/v1/auth/sign-up
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subdub API");
